f5d469405705c0c98e966089d06009354f4dc8e0
haks1999 <haks1999@gmail.com>
2015-06-03 10:27:10 +0900

    일단 app 쪽 header랑 footer 가튼거 정리

--- server/app/routes/app.server.routes.js
+++ server/app/routes/app.server.routes.js
@@ -3,9 +3,4 @@ var appServerController = require('../controllers/app.server.controller');
 module.exports = function(app) {
 	app.get('/', appServerController.renderIndex);
 
-	app.get('/app', appServerController.renderApp);
-
-	app.get('/blocks/header', appServerController.renderHeader);
-
-	app.get('/blocks/settings', appServerController.renderSettings);
 };
