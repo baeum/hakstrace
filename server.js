760731396b7356155551186186c78ca907fbaa14
Gunhlee Lee <gunlee01@gmail.com>
2015-06-09 22:28:02 +0900

    simplify server.js

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 var httpPort = process.env.HAKSTRACE_WEB_PORT || 3000;
 
-/*
-var mongoose = require('./config/mongoose'),
-	express = require('./config/express'),
-	passport = require('./config/passport');
-	*/
+var mongoose = require('./config/mongoose');
+var express = require('./config/express');
 
-var mongoose = require('./config/mongoose'),
-	express = require('./config/express'),
-	passport = require('./config/passport');
-
-var db = mongoose();	// 이게 먼저 되야됨. db 니깐
-//var app = express(db);
+var db = mongoose();	// db first
 var app = express(db);
-var passport = passport();
+require('./config/passport')();
 
 app.listen(httpPort);
-//module.exports = app;
 
 console.log('Server running at http://localhost:%d/', httpPort);
