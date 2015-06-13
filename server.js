ace3fe7bf467682ae85cc0f8c4aba217281fee36
Gunhlee Lee <gunlee01@gmail.com>
2015-06-13 21:14:40 +0900

    simplify something

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
 var httpPort = process.env.HAKSTRACE_WEB_PORT || 3000;
 
-var mongoose = require('./config/mongoose');
-var express = require('./config/express');
+require('./config/mongoose')();
 
-var db = mongoose();	// db first
-var app = express(db);
+var app = require('./config/express')();
+
 require('./config/passport')();
 
 app.listen(httpPort);
