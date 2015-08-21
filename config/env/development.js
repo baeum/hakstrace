8bbb8c47d091a146759726ad15de1a290fdbc0d9
Gunhee Lee <gunlee01@gmail.com>
2015-08-21 15:51:32 +0900

    prototype of nav timing

--- config/env/development.js
+++ config/env/development.js
@@ -7,6 +7,6 @@ module.exports = {
 	mongoDebug: 'default',
 	sessionSecret: 'developmentSessionSecret',
 
-	mainDashboardInterval: 5000, //interval for development(longer on production)
-	mainDashboardLazy: 10000
+	mainDashboardInterval: 1000, //interval for development(longer on production)
+	mainDashboardLazy: 3000
 };
